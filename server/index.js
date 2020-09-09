3c2b02c884a84b9327eec3a80667644f19aa60e5
Komchan Khumphai <joekim@odds.team>
2020-09-09 16:24:20 +0700

    changed deploy config

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const pgClient = new Pool({
 
 pgClient.on('connect', () => {
   pgClient
-    .query('CREATE TABLE IF NOT EXISTS fibvalues (number INT)')
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch((err) => console.log(err));
 });
 
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from fibvalues');
+  const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
@@ -59,9 +59,9 @@ app.post('/values', async (req, res) => {
     return res.status(422).send('Index too high');
   }
 
-  redisClient.hset('fibvalues', index, 'Nothing yet!');
+  redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO fibvalues(number) VALUES($1)', [index]);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
   res.send({ working: true });
 });
